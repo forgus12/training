a0ac30e5645350cd317548429b9f09660eb55008
forgus12 <74146784+forgus12@users.noreply.github.com>
2020-11-20 18:17:14 +0300

    Add files via upload

---
+++ sumWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace sumWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddNumbers = 0;
+            int evenNumbers = 0;
+
+            Console.WriteLine("Введите первое число диапазона");
+
+            int firstNumberRange = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите последнее число диапазона");
+
+            int secondNumberRange = int.Parse(Console.ReadLine());
+
+            while (firstNumberRange<=secondNumberRange)
+            {
+                if ((firstNumberRange % 2)== 0)
+                {
+                    evenNumbers++;
+                }
+                else
+                {
+                    oddNumbers++;
+                }
+                firstNumberRange++;
+            }
+            Console.WriteLine("Четных чисел" + evenNumbers);
+            Console.WriteLine("Не четных чисел" + oddNumbers);
+        }
+    }
+}
---
+++ two/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+
+            Console.WriteLine("Введите первое число");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue) / 2;
+
+            Console.WriteLine("Среднее арифметическое равно " +result);
+
+        }
+    }
+}
