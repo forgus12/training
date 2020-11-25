d7153015cdff88651a02db1ff14950d4af8e7239
forgus <74146784+forgus12@users.noreply.github.com>
2020-11-25 22:03:26 +0300

    1

---
+++ SearchIndex/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SearchIndex
+{
+    class Program
+    {
+
+        static int SearchIn(int[] arr,int a)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]==a)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 15, 3, 42, 515, 42 };
+            Console.WriteLine(SearchIn(myArray, 5));
+        }
+    }
+}
---
+++ method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Print(string symbol,int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine(symbol);
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ(-ы) который(-е) хотите вывести");
+            string sym= Console.ReadLine();
+            Console.WriteLine("Введите сколько раз вы хотите вывести ");
+            int num = int.Parse(Console.ReadLine());
+            Print(sym, num);
+        }
+    }
+}
