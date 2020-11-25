d6408e32d9b55f48c2b5c5f4b759e9f10ca22e62
forgus <74146784+forgus12@users.noreply.github.com>
2020-11-25 19:30:22 +0300

    1

---
+++ ArrayTwoDimensional/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayTwoDimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[3, 5];
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine("Y "+ i + " X "+ j);
+                    myArray[i,j]=int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
