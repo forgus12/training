f69be2dc0049311de69b028d89618fa97b9b9aba
forgus <74146784+forgus12@users.noreply.github.com>
2020-11-25 20:21:31 +0300

    1

---
+++ ArrayThreeDimensional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArrayTwoDimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] myArray = new int[4,3, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        myArray[i, j,k] = random.Next(199);
+                    }
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                Console.WriteLine("№ "+ (i +1));
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        Console.Write(myArray[i, j,k] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("=========");
+            }
+        }
+    }
+}
