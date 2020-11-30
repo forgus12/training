e6ee07bd56a73d052dc28fa66fb3b55d95dfd801
forgus <74146784+forgus12@users.noreply.github.com>
2020-11-30 22:51:37 +0300

    1

---
+++ OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP
+{
+
+    class Program
+    {
+        static TeamKVN GetTeam()
+        {
+            TeamKVN team = new TeamKVN();
+            team.name = "Алоха";
+            team.commandStructure = "3 парней и 2 девушек";
+            team.averageAge = 19;
+            team.yearOfCreation = 2008;
+            team.captainTeam = "Петров Петр";
+            return team;
+        }
+
+        static void Print(TeamKVN team)
+        {
+            Console.WriteLine($"Название команды КВН :{team.name }");
+            Console.WriteLine($"Команда состоит из :{team.commandStructure }");
+            Console.WriteLine($"Средний возвраст команды :{ team.averageAge}");
+            Console.WriteLine($"Год создания команды :{team.yearOfCreation }");
+            Console.WriteLine($"Капитан команды :{ team.captainTeam}");
+        }
+
+        static void Main(string[] args)
+        {
+            var team = GetTeam();
+
+            Print(team);
+        }
+    }
+}
---
+++ OOP/TeamKVN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class TeamKVN
+    {
+        public string name;
+        public string commandStructure;
+        public int averageAge;
+        public int yearOfCreation;
+        public string captainTeam;
+    }
+}
